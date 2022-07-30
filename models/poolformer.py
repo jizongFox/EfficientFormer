5e9740ab6c5ab4e70075c29f5908552755194976
jizong <jizong.peng.1@etsmtl.net>
2022-07-30 16:01:13 -0400

    added MSHA into poolformer.py

--- models/poolformer.py
+++ models/poolformer.py
@@ -62,6 +62,48 @@ default_cfgs = {
 }
 
 
+class Attention(nn.Module):
+    """Attention module that can take tensor with [B, N, C] or [B, C, H, W] as input.
+    Modified from:
+    https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+    """
+
+    def __init__(self, dim, head_dim=32, qkv_bias=False, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        assert dim % head_dim == 0, 'dim should be divisible by head_dim'
+        self.head_dim = head_dim
+        self.num_heads = dim // head_dim
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        shape = x.shape
+        if len(shape) == 4:
+            B, C, H, W = shape
+            N = H * W
+            x = torch.flatten(x, start_dim=2).transpose(-2, -1)  # (B, N, C)
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)  # make torchscript happy (cannot use tensor as tuple)
+
+        # trick here to make q@k.t more stable
+        attn = (q * self.scale) @ k.transpose(-2, -1)
+        # attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        if len(shape) == 4:
+            x = x.transpose(-2, -1).reshape(B, C, H, W)
+
+        return x
+
+
 class PatchEmbed(nn.Module):
     """
     Patch Embedding that is implemented by a layer of conv.
